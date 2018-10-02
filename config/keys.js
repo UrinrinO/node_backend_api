1e492c80a57fe84356f97f107b9dc284a13fba4b
UrinrinO <urinrinogidiama@gmail.com>
2018-10-02 14:50:47 +0100

    Initial Express server & Routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://root:password1!@ds119993.mlab.com:19993/dev_connector"
+};
---
+++ routes/api/posts.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// @routes GET api/posts/test
+// @desc Tests Posts routes
+// @access Public
+router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+module.exports = router;
